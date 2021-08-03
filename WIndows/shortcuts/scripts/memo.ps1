838b11059206e550cb904d51cd11c875ff19de48
Akiemon <36184922+sibafb@users.noreply.github.com>
2021-08-04 00:16:24 +0900

    Update memo.ps1

--- WIndows/shortcuts/scripts/memo.ps1
+++ WIndows/shortcuts/scripts/memo.ps1
@@ -1,8 +1,19 @@
 $date = Get-Date -Format "yyyyMMdd_HHmm"
 $dest = Join-Path $env:HOMEPATH\_WS\_memo ($date + ".txt")
+$defaultcontent = ("■"+$date+" memo ")
 
 if (-not (Test-Path $dest)){
-  New-Item $dest
- }
-echo ("■"+$date+" memo ") >> $dist
+        New-Item $dest
+        echo $defaultcontent >> $dest
+}
+
 vim $dest
+
+$content = Get-Content $dest
+if ( $content -eq ("■"+$date+" memo ") ){
+        Add-Type -Assembly System.Windows.Forms
+        $result =[System.Windows.Forms.MessageBox]::Show("ファイルを削除します。", "info","YesNo")
+        If($result -eq "Yes" ){
+                Remove-Item $dest
+        }
+}
